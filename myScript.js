d995cf9df78cb5592f68a2af33dc5b33f6489ab8
beatmil <anutorn@gmail.com>
2020-07-06 23:56:34 +0700

    initial commit

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function set_maika2() {
+  document.getElementById("bob2").innerHTML = "Maika is so kyuut >///<";
+}
+function pop_up() {
+    alert("Buy Maika figure!!!")
+}
+function random_number() {
+  return Math.floor(Math.random() * 11); // random numbers from 0 - 10
+}
+function getRndInteger(min, max) { // a proper random function from w3schools
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
